6422030f0760c6d8d7d7b5d1f2a29f92dac4f89a
0v1se <dev@0v1se.com>
2018-07-30 17:06:27 +0300

    added test controller

---
+++ src/main/java/io/daonomic/kyc/controller/TestController.java
@@ -0,0 +1,12 @@
+package io.daonomic.kyc.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/")
+    public String test() {
+        return "It's working";
+    }
+}
